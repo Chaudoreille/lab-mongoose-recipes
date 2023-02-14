4149f3d477657c655e3baccb8b91ca8481507cc1
Chaudoreille <a.chaudoreille@gmail.com>
2023-02-14 14:28:37 +0100

    apply TA corrections & recommendations

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = 'mongodb://127.0.0.1/recipe-app';
 
 // Connection to the database "recipe-app"
 mongoose
@@ -16,8 +16,8 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
-  .then(() => {
-    return Recipe.create({
+  .then(async () => {
+    const myRecipe = await Recipe.create({
       "title": "Corn Flakes and Milk",
       "level": "Easy Peasy",
       "ingredients": [
@@ -30,22 +30,21 @@ mongoose
       "duration": 1,
       "creator": "Chef Aipamferchillais"
     })
-  })
-  .then(() => {
-      return Recipe.insertMany(data)
-  })
-  .then(async () =>  {
+    console.log("\n", myRecipe.title, "\n")
+
+    const batchInsert = await Recipe.insertMany(data)
+    batchInsert.forEach(recipe => console.log(recipe.title))
+
     const rigatoni = await Recipe.findOneAndUpdate(
       {title: "Rigatoni alla Genovese"},
       {duration: 100},
       {new: true}
-      )
-      console.log(`${rigatoni.title} recipe is now ${rigatoni.duration}mn long, as it should be.`)
-  })
-  .then(async () => {
+    )
+    console.log("\n", `${rigatoni.title} recipe is now ${rigatoni.duration}mn long, as it should be.`, "\n")
+    
     const missionReport = await Recipe.deleteOne({title: "Carrot Cake"})
     if (missionReport.deletedCount === 1) {
-      console.log("Carrot Cake is down! Over.")
+      console.log("Carrot Cake is down! Over.", "\n")
     }
   })
   .catch(error => {
